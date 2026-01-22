d7433bde92f82ef00084c2327c7b4ce963759add
Maria Belen Gallo <hellmaker3d@gmail.com>
2026-01-22 17:56:07 -0300

    correccion de layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+import { ReactNode } from 'react'
+import SideNav from '@/app/ui/dashboard/sidenav'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className="grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
         </div>
-    );
+    )
 }
