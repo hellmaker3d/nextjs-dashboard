41de036726f95deec66fcb7e4c565e5638356573
Maria Belen Gallo <hellmaker3d@gmail.com>
2026-01-27 17:03:54 -0300

    se agrego loeading y suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
       <body>
-        <h1>HOLA SOY MILO</h1>
         {children}
       </body>
     </html>
