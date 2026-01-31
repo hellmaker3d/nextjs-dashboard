be107c6b9a735daea68d06a695b6aeda2659a174
Maria Belen Gallo <hellmaker3d@gmail.com>
2026-01-31 20:11:27 -0300

    se agrego status, edit and delete

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,42 @@
-'use client';
-
+'use client';// app/ui/search.tsx
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); //  Get current search parameters
+  const pathname = usePathname();        // Get current path name
+  const replace = useRouter();          //  Get router for navigation
+
+
+  const handleSearch = useDebouncedCallback((term) => { //  Handle search input changes
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');//  Reset to first page on new search
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace.replace(`${pathname}?${params.toString()}`);//  Update URL with new search parameters 
+  }, 300);//  Debounce for 300ms
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
+
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}// Set default value from URL search params
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
     </div>
   );
 }
